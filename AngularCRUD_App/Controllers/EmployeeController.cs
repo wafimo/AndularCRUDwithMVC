c86a4de4f8962d1d570ca19067a0aec8486d93ef
wafimo <omix02@gmail.com>
2017-12-27 12:51:31 +0600

    Application Upload

---
+++ AngularCRUD_App/Controllers/EmployeeController.cs
@@ -0,0 +1,114 @@
+﻿using AngularCRUD_App.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace AngularCRUD_App.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee  
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>  
+        ///   
+        /// Get All Employee  
+        /// </summary>  
+        /// <returns></returns>  
+        public JsonResult Get_AllEmployee()
+        {
+            using (EmployeeEntities Obj = new EmployeeEntities())
+            {
+                List<Employee> Emp = Obj.Employees.ToList();
+                return Json(Emp, JsonRequestBehavior.AllowGet);
+            }
+        }
+        /// <summary>  
+        /// Get Employee With Id  
+        /// </summary>  
+        /// <param name="Id"></param>  
+        /// <returns></returns>  
+        public JsonResult Get_EmployeeById(string Id)
+        {
+            using (EmployeeEntities Obj = new EmployeeEntities())
+            {
+                int EmpId = int.Parse(Id);
+                return Json(Obj.Employees.Find(EmpId), JsonRequestBehavior.AllowGet);
+            }
+        }
+        /// <summary>  
+        /// Insert New Employee  
+        /// </summary>  
+        /// <param name="Employe"></param>  
+        /// <returns></returns>  
+        public string Insert_Employee(Employee Employe)
+        {
+            if (Employe != null)
+            {
+                using (EmployeeEntities Obj = new EmployeeEntities())
+                {
+                    Obj.Employees.Add(Employe);
+                    Obj.SaveChanges();
+                    return "Employee Added Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Inserted! Try Again";
+            }
+        }
+        /// <summary>  
+        /// Delete Employee Information  
+        /// </summary>  
+        /// <param name="Emp"></param>  
+        /// <returns></returns>  
+        public string Delete_Employee(Employee Emp)
+        {
+            if (Emp != null)
+            {
+                using (EmployeeEntities Obj = new EmployeeEntities())
+                {
+                    var Emp_ = Obj.Entry(Emp);
+                    if (Emp_.State == System.Data.Entity.EntityState.Detached)
+                    {
+                        Obj.Employees.Attach(Emp);
+                        Obj.Employees.Remove(Emp);
+                    }
+                    Obj.SaveChanges();
+                    return "Employee Deleted Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Deleted! Try Again";
+            }
+        }
+        /// <summary>  
+        /// Update Employee Information  
+        /// </summary>  
+        /// <param name="Emp"></param>  
+        /// <returns></returns>  
+        public string Update_Employee(Employee Emp)
+        {
+            if (Emp != null)
+            {
+                using (EmployeeEntities Obj = new EmployeeEntities())
+                {
+                    var Emp_ = Obj.Entry(Emp);
+                    Employee EmpObj = Obj.Employees.Where(x => x.Emp_Id == Emp.Emp_Id).FirstOrDefault();
+                    EmpObj.Emp_Age = Emp.Emp_Age;
+                    EmpObj.Emp_City = Emp.Emp_City;
+                    EmpObj.Emp_Name = Emp.Emp_Name;
+                    Obj.SaveChanges();
+                    return "Employee Updated Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Updated! Try Again";
+            }
+        }
+    }
+}
---
+++ AngularCRUD_App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularCRUD_App.MvcApplication" Language="C#" %>
